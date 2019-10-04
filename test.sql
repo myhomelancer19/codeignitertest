90d62812dddbe331e020c59c84fb8664baa03c13
myhomelancer19 <myhomelancer19@outlook.com>
2019-10-05 02:42:19 +0900

    first commit

---
+++ test.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `student` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) NOT NULL,
+ `sex` varchar(255) NOT NULL,
+ `class_id` int(11) NOT NULL,
+ `created_at` datetime NOT NULL,
+ `updated_at` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
+
+CREATE TABLE `class` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) NOT NULL,
+ `created_at` datetime NOT NULL,
+ `updated_at` datetime NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
+
+# database name : demo defined in application/config/database.php
